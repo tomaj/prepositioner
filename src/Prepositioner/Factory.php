b131e6a3fd3103fe9712d3cf26f0897b193441ae
Tomas Majer <tomasmajer@gmail.com>
2014-07-04 09:03:19 +0200

    Added factory support for creating prepositioner with escape string

--- src/Prepositioner/Factory.php
+++ src/Prepositioner/Factory.php
@@ -4,12 +4,12 @@ namespace Tomaj\Prepositioner;
 
 class Factory
 {
-    public static function build($language)
+    public static function build($language, $escapeString = '#####')
     {
         $className = '\Tomaj\Prepositioner\\' . ucfirst($language) . 'Language';
         if (class_exists($className)) {
             $language = new $className;
-            return new Prepositioner($language->prepositions());
+            return new Prepositioner($language->prepositions(), $escapeString);
         }
 
         throw new LanguageNotExistsException("Language class '$className' doesn't exists");
