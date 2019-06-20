6734b33c30739c3507bb536aa59176e3d3ce9ba0
Samuel kelemen <kelemen@efabrica.sk>
2019-06-20 11:42:39 +0200

    Added romanian language

---
+++ src/Prepositioner/Languages/RomanianLanguage.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tomaj\Prepositioner;
+
+class RomanianLanguage implements LanguageInterface
+{
+    public function prepositions(): array
+    {
+        return [
+            /* 2 letter */
+            'cu',
+            'de',
+            'în',
+            'la',
+            'pe',
+
+            /* 3 letter */
+            'cât',
+            'pro',
+
+            /* 4 letter */
+            'fără',
+            'până',
+            'prin',
+            'spre',
+
+            /* 5 letter */
+            'între',
+            'peste',
+
+            /* 6 letter */
+            'dintre',
+            'pentru',
+        ];
+    }
+}
